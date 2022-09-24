0011fd0017e20acb30210cf90182c6f4d3a502c1
tatuan0312 <85239207+tatvlogs0312@users.noreply.github.com>
2022-09-24 08:07:22 +0700

    update web th01

---
+++ Web-TH01/config.php
@@ -0,0 +1,14 @@
+<?php
+/* Thông tin CSDL. Giả sử bạn đang chạy MySQL Server với thiết lập mặc định (user 'root' và không có mật khẩu) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'thweb01');
+ 
+/* Cố gắng kết nối với cơ sở dữ liệu MySQL */
+$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Kiểm tra kết nối
+if($mysqli === false){
+    die("ERROR: Không thể kết nối. " . $mysqli->connect_error);
+}
