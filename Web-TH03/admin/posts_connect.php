bd91f1b176f3c069d01e4795897f6c134833cfff
tatuan0312 <85239207+tatvlogs0312@users.noreply.github.com>
2022-10-05 19:12:48 +0700

    TH03

---
+++ Web-TH03/admin/posts_connect.php
@@ -0,0 +1,3 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "data");
+mysqli_set_charset($conn, 'UTF8');
---
+++ Web-TH03/footer.php
@@ -0,0 +1,20 @@
+<div class="jumbotron">
+
+    <div class="appeal">
+    </div>
+
+
+    <h2>Copyright @ 2020 Design by Trần Anh Tuấn</h2>
+
+</div>
+
+</div>
+
+<!--/. Container-->
+</div>
+
+</div>
+
+</body>
+
+</html>
